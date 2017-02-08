3e8045076dda7cffc9ae38a207621ec2650ef9cc
galinaVP <galka.prit@gmail.com>
2017-02-08 02:12:23 -0800

    Lesson2-Done

---
+++ lesson2/task2_Prytuliak/task2_Prytuliak/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2_Prytuliak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iNumber=0, iRemainder=0;
+            
+
+            while (iNumber++ < 100)
+            {
+                if (iNumber % 5 == iRemainder && iNumber % 3 == iRemainder)
+                {
+                    Console.WriteLine("{0}", "FizzBuzz");
+                }
+                else if (iNumber % 3 == iRemainder)
+                {
+                   Console.WriteLine("{0}", "Fizz");
+                }
+                else if (iNumber % 5 == iRemainder)
+                {
+                   Console.WriteLine("{0}", "Buzz");
+                 }
+              
+                else
+                {
+                    Console.WriteLine(iNumber);
+                }
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
