e5e5d7886ebfb8375e15d549718fa3138b99f176
galinaVP <galka.prit@gmail.com>
2017-02-12 04:06:39 -0800

    Lesson3-Enter values, show all to user

--- lesson3/Lesson3/Lesson3/Program.cs
+++ lesson3/Lesson3/Lesson3/Program.cs
@@ -10,13 +10,27 @@ namespace Lesson3
     {
         static void Main(string[] args)
         {
-            int iArraySize;
-            int [] iArray;
+            int iArraySize, i;
+            int[] iArray = new int[10];
 
 
             Console.WriteLine("Enter array size:");
             iArraySize = Int32.Parse(Console.ReadLine());
 
+            Console.WriteLine("Fill array with numbers. After each number click Enter button");
+       
+            for (i = 0; i < iArraySize; i++)
+            {
+                Console.Write("Enter {0}:", i);
+                iArray[i] = Int32.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("You entered next values:");
+            for (i = 0; i < iArraySize; i++)
+            {
+                Console.WriteLine(iArray[i]);
+            }
             Console.ReadLine();
         }
         
