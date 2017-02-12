3069797920726822f65af6f6377987e99173f7df
galinaVP <galka.prit@gmail.com>
2017-02-12 03:30:38 -0800

    Lesson3-Enter array size

---
+++ lesson3/Lesson3/Lesson3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iArraySize;
+            int [] iArray;
+
+
+            Console.WriteLine("Enter array size:");
+            iArraySize = Int32.Parse(Console.ReadLine());
+
+            Console.ReadLine();
+        }
+        
+    }
+}
