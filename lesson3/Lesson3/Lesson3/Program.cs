93d3d81def4d4efd7596fc28a88736f43dd0d7f6
galinaVP <galka.prit@gmail.com>
2017-02-14 06:15:11 -0800

    Lesson3_DONE_min_max_divided to different methods

--- lesson3/Lesson3/Lesson3/Program.cs
+++ lesson3/Lesson3/Lesson3/Program.cs
@@ -8,42 +8,64 @@ namespace Lesson3
 {
     class Program
     {
+        static int iMax, iMin, i; 
+                
         static void Main(string[] args)
         {
             Console.WriteLine("Enter Array size in range 1-214748300:");
             int iArraySize = Int32.Parse(Console.ReadLine());
-                    
+
             int[] iArray = new int[iArraySize];
-            int i, iMax, iMin;
-                    
+            
             Console.WriteLine("Fill array with numbers. After each number click Enter button");
-       
+
             for (i = 0; i < iArraySize; i++)
             {
                 Console.Write("Enter {0}:", i);
                 iArray[i] = Int32.Parse(Console.ReadLine());
             }
-    
-            iMax = iArray[0];
-            iMin = iArray[0];
 
-            for (i = 0; i < iArraySize; i++)
+            int iMax = FindMax(iArray);
+            int iMin = FindMin(iArray);
+
+            Console.WriteLine("Maximum value is: {0}", iMax);
+            Console.WriteLine("Minimum value is: {0}", iMin);
+            Console.ReadLine();
+
+        }
+
+            static int FindMax(int[] iArray)
             {
-                if (iMax < iArray[i]) 
+
+            iMax = iArray[0];
+
+            for (i = 0; i < iArray.Length; i++)
+            {
+                if (iMax < iArray[i])
                 {
                     iMax = iArray[i];
                 }
+             }
+            return iMax;
+            }
+
+
+        static int FindMin(int[] iArray)
+        {
+
+           iMin = iArray[0];
+
+            for (i = 0; i < iArray.Length; i++)
+            {
                 if (iMin > iArray[i])
                 {
                     iMin = iArray[i];
                 }
             }
-        
-            Console.WriteLine("Maximum value is: {0}", +iMax);
-            Console.WriteLine("Minimum value is: {0}", +iMin);
-            
-            Console.ReadLine();
+            return iMin;
         }
+          
+       
         
     }
 }
